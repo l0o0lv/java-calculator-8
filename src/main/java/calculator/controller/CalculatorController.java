d1d1ece941e00d48047b04d748f48a8abafed282
한동근 <ehdrms001030@naver.com>
2025-10-19 10:57:33 +0900

    feat: 문자열을 입력받는 기능 구현 - 시작 메시지를 상수 클래스에 선언

---
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -0,0 +1,17 @@
+package calculator.controller;
+
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
+public class CalculatorController {
+    private static String str = "";
+
+    public static void run(){
+        inputString();
+    }
+
+    public static void inputString(){
+        OutputView.startCalculator();
+        str = InputView.getInput();
+    }
+}
